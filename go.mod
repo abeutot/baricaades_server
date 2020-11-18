89ecc4f41839fb0ff5f1fd7abf17afd0d1610187
Anaël Beutot <abeutot@oserror.org>
2020-11-19 00:33:06 +0100

    Add CORS support

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/abeutot/baricaades_server
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/namsral/flag v1.7.4-pre // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/namsral/flag"
 )
 
 // POST /user/register
@@ -28,7 +32,19 @@ func getGame(c *gin.Context) {
 }
 
 func main() {
+	var corsAllowOrigins string
+
+	flag.StringVar(&corsAllowOrigins, "cors_allow_origins", "http://localhost:3000", "Origins to allow with CORS (comma separated)")
+	flag.Parse()
+
 	r := gin.Default()
+
+	/* CORS config */
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: strings.Split(corsAllowOrigins, ","),
+		AllowMethods: []string{"GET", "POST"},
+		MaxAge:       24 * time.Hour,
+	}))
 
 	r.GET("/game/42", getGame)
 
